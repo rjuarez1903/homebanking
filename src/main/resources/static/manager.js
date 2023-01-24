732df71561bc123279e4f09077e11ce7d91f6a4e
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-24 18:55:13 -0300

    commit initial files

---
+++ src/main/resources/static/manager.js
@@ -0,0 +1,74 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            regExMail:    /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/,
+            json:         {},
+            clients:      [],
+            firstName:     '',
+            lastName:     '',
+            email:        '',
+            clientDetail: {},
+            firstNameEdit: '',
+            lastNameEdit: '',
+            emailEdit:    '',
+        }
+    },
+    created() {
+        this.loadData()
+    },
+    methods: {
+        loadData() {
+            axios.get('/clients')
+                .then(response => {
+                    this.json    = response
+                    this.clients = response.data._embedded.clients
+                })
+                .catch(error => console.log(error))
+        },
+        getClientId(client) {
+            return client._links.client.href.split("/").slice(-1).toString()
+        },
+        addClient() {
+            if ((this.firstName != '' && this.lastName != '') && (this.regExMail.test(this.email))) {
+                this.postClient()
+            }
+        },
+        postClient() {
+            axios.post('/clients', {
+                firstName: this.firstName,
+                lastName: this.lastName,
+                email:    this.email
+            })
+                .then(() => this.loadData()
+                )
+                .catch(error => console.log(error))
+        },
+        showClient(id) {
+            this.clientDetail = this.clients.filter(client => this.getClientId(client) == id)[0]
+            axios.get(`/clients/${id}`)
+                .then(response => {
+                    this.firstNameEdit = response.data.firstName
+                    this.lastNameEdit = response.data.lastName
+                    this.emailEdit    = response.data.email
+                })
+        },
+        editClient(id) {
+            if ((this.firstNameEdit != '' && this.lastNameEdit != '') && (this.regExMail.test(this.emailEdit))) {
+                axios.put(`/clients/${id}`, {
+                    firstName: this.firstNameEdit,
+                    lastName: this.lastNameEdit,
+                    email:    this.emailEdit,
+                })
+                    .then(() => this.loadData())
+                    .catch(error => console.log(error))
+            }
+        },
+        deleteClient(id) {
+            axios.delete(`/clients/${id}`)
+                .then(() => this.loadData())
+                .catch(error => console.log(error))
+        }
+    }
+}).mount('#app')
