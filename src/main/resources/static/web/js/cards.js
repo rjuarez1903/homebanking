43157149ac6dc4eec06d701a5d03f75d56075538
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-02-01 17:56:51 -0300

    update gitignore, style changes

--- src/main/resources/static/web/js/cards.js
+++ src/main/resources/static/web/js/cards.js
@@ -13,12 +13,21 @@ createApp({
         loadData() {
             axios("/api/clients/1")
                 .then(response => this.cards = response.data.cards)
-                .then(() => console.log(this.cards))
                 .catch(error => console.log(error))
         },
         formatCardNumber(number) {
             return number.match(/.{1,4}/g).join(" ")
             
+        }, 
+        getStringDate(date) {
+            const year = new Date(date).getFullYear().toString().slice(2)
+            let month = new Date(date).getMonth() + 1
+            if (month < 10) {
+                month = month.toString().padStart(2, '0');
+            } else {
+                month = month.toString()
+            }
+            return `${month}/${year}`
         }
     }
 }).mount('#app')
