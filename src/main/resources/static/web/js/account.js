6d62c3ca2adfaa6c7dd3f7b952b6ba40ed1961e7
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-02-02 11:45:44 -0300

    add new table style

--- src/main/resources/static/web/js/account.js
+++ src/main/resources/static/web/js/account.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            client:       {},
             account:      {},
             transactions: []
         }
@@ -12,6 +13,9 @@ createApp({
     },
     methods: {
         loadData() {
+            axios('/api/clients/1')
+                .then(response => this.client = response.data)
+                .catch(error => console.log(error))
             const urlParams = new URLSearchParams(window.location.search)
             const id =  urlParams.get('id')
             axios(`/api/accounts/${id}`)
@@ -19,6 +23,11 @@ createApp({
                 .then(() => this.transactions = this.account.transactions)
                 .then(() => this.sortByDate(this.transactions))
                 .catch(error => console.log(error))
+        },
+        toggleMenu(e) {
+            const menu = document.querySelector('aside')
+            e.preventDefault()
+            menu.classList.toggle('toggle-menu')
         },
         getDate(date) {
             return new Date(date).toLocaleString()
