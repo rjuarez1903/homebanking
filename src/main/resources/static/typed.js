8ce3e0d81949184fdd51d760dd0237516df04925
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-02-07 20:21:56 -0300

    change images location, delete typed js script

--- src/main/resources/static/typed.js
+++
@@ -1,7 +0,0 @@
-// const typed = new Typed('#typed',{
-//     strings:["life", "finances", "business", "life"],
-//     backSpeed: 120,
-//     typeSpeed: 120,
-//     loop: true,
-//     // shuffle: true
-// });
