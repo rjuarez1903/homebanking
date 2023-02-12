a12384537246d26541dc19c5ba5744375b5a9571
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-02-12 17:41:51 -0300

    add card entity

---
+++ src/main/java/com/mindhub/homebanking/dtos/CardDTO.java
@@ -0,0 +1,61 @@
+package com.mindhub.homebanking.dtos;
+
+import com.mindhub.homebanking.models.Card;
+import com.mindhub.homebanking.models.CardColor;
+import com.mindhub.homebanking.models.CardType;
+
+import java.time.LocalDateTime;
+
+public class CardDTO {
+    private long id;
+    private String cardholder;
+    private CardType type;
+    private CardColor color;
+    private String number;
+    private String cvv;
+    private LocalDateTime fromDate;
+    private LocalDateTime thruDate;
+
+    public CardDTO(Card card) {
+        this.id = card.getId();
+        this.cardholder = card.getCardholder();
+        this.type = card.getType();
+        this.color = card.getColor();
+        this.number = card.getNumber();
+        this.cvv = card.getCvv();
+        this.fromDate = card.getFromDate();
+        this.thruDate = card.getThruDate();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCardholder() {
+        return cardholder;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public CardColor getColor() {
+        return color;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public LocalDateTime getFromDate() {
+        return fromDate;
+    }
+
+    public LocalDateTime getThruDate() {
+        return thruDate;
+    }
+}
---
+++ src/main/java/com/mindhub/homebanking/models/Card.java
@@ -0,0 +1,105 @@
+package com.mindhub.homebanking.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native" )
+    @GenericGenerator(name  = "native", strategy = "native")
+    private long id;
+    private String cardholder;
+    private CardType type;
+    private CardColor color;
+    private String number;
+    private String cvv;
+    private LocalDateTime fromDate;
+    private LocalDateTime thruDate;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Card() {};
+
+    public Card(CardType type, CardColor color, String number, String cvv, LocalDateTime fromDate, LocalDateTime thruDate, Client client) {
+        this.cardholder = client.getFirstName() + " " + client.getLastName();
+        this.type = type;
+        this.color = color;
+        this.number = number;
+        this.cvv = cvv;
+        this.fromDate = fromDate;
+        this.thruDate = thruDate;
+        this.client = client;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCardholder() {
+        return cardholder;
+    }
+
+    public void setCardholder(String cardholder) {
+        this.cardholder = cardholder;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public void setType(CardType type) {
+        this.type = type;
+    }
+
+    public CardColor getColor() {
+        return color;
+    }
+
+    public void setColor(CardColor color) {
+        this.color = color;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public LocalDateTime getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(LocalDateTime fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public LocalDateTime getThruDate() {
+        return thruDate;
+    }
+
+    public void setThruDate(LocalDateTime thruDate) {
+        this.thruDate = thruDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
