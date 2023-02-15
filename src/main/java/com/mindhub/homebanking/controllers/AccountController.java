162f47275919667fc75d1f1c3b29c384fb6d1f64
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-02-15 17:52:39 -0300

    add create account method

--- src/main/java/com/mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/homebanking/controllers/AccountController.java
@@ -1,13 +1,22 @@
 package com.mindhub.homebanking.controllers;
 
 import com.mindhub.homebanking.dtos.AccountDTO;
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.AccountRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static java.util.stream.Collectors.toList;
 
@@ -16,7 +25,9 @@ import static java.util.stream.Collectors.toList;
 public class AccountController {
 
     @Autowired
-    private AccountRepository accountRepository;
+    AccountRepository accountRepository;
+    @Autowired
+    ClientRepository clientRepository;
 
     @RequestMapping("/accounts")
     public List<AccountDTO> getAccounts() {
@@ -26,5 +37,19 @@ public class AccountController {
     @RequestMapping("/accounts/{id}")
     public AccountDTO getAccount(@PathVariable Long id) {
         return accountRepository.findById(id).map(AccountDTO::new).orElse(null);
+    }
+
+    @RequestMapping(path = "/clients/current/accounts", method = RequestMethod.POST)
+    public ResponseEntity<Object> createAccount(Authentication authentication) {
+        Client client = clientRepository.findByEmail(authentication.getName());
+        if (client.getAccounts().size() < 3) {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, 99999999 + 1);
+            Account account =  new Account("VIN" + randomNum, LocalDateTime.now(), 0);
+            client.addAccount(account);
+            accountRepository.save(account);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("Can't generate more than 3 accounts per client.", HttpStatus.FORBIDDEN);
+        }
     }
 }
