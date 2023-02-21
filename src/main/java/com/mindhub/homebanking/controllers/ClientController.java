a3f9fabc2cc5a1d38fe6a5d6e4d4e271a97ffa54
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-02-21 18:41:28 -0300

    add validations

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -40,8 +40,20 @@ public class ClientController {
                                            @RequestParam String email,
                                            @RequestParam String password) {
 
-        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()) {
-            return new ResponseEntity<>("Missing data", HttpStatus.FORBIDDEN);
+        if (firstName.isEmpty()) {
+            return new ResponseEntity<>("Missing first name.", HttpStatus.FORBIDDEN);
+        }
+
+        if (lastName.isEmpty()) {
+            return new ResponseEntity<>("Missing last name.", HttpStatus.FORBIDDEN);
+        }
+
+        if (email.isEmpty()) {
+            return new ResponseEntity<>("Missing email", HttpStatus.FORBIDDEN);
+        }
+
+        if (password.isEmpty()) {
+            return new ResponseEntity<>("Missing password", HttpStatus.FORBIDDEN);
         }
 
         if (clientRepository.findByEmail(email) != null) {
