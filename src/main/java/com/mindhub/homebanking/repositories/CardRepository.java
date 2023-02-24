0f92517d1c7285da707a81671f8d3d937a63b9c2
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-02-24 18:57:30 -0300

    add findAllByExpired method

--- src/main/java/com/mindhub/homebanking/repositories/CardRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/CardRepository.java
@@ -6,8 +6,11 @@ import com.mindhub.homebanking.models.CardType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CardRepository extends JpaRepository<Card, Long> {
     Card findByNumber(String number);
     Card findByCardholderAndTypeAndColorAndExpired(String cardholder, CardType type, CardColor color, Boolean expired);
+    List<Card> findAllByExpired(Boolean expired);
 }
