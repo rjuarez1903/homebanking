53faed887f3eabb9df51849e9b10be26cd0793c1
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-03-01 13:39:56 -0300

    change WebSecurityConfigurerAdapter

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
@@ -1,10 +1,11 @@
 package com.mindhub.homebanking.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
@@ -14,10 +15,10 @@ import javax.servlet.http.HttpSession;
 
 @EnableWebSecurity
 @Configuration
-public class WebAuthorization extends WebSecurityConfigurerAdapter {
+public class WebAuthorization {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/api/clients/current/**", "/api/loans").hasAnyAuthority("ADMIN", "CLIENT")
             .antMatchers(HttpMethod.POST, "/api/clients/**").permitAll()
@@ -45,6 +46,8 @@ public class WebAuthorization extends WebSecurityConfigurerAdapter {
         http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
 
         http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+
+        return http.build();
     }
 
     private void clearAuthenticationAttributes(HttpServletRequest request) {
