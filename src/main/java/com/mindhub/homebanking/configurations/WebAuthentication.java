41bc70a48dcee45fc6072d1c18e6b7f724a4ff87
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-03-02 00:25:39 -0300

    change spacing

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthentication.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-
 public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
 
     @Autowired
@@ -23,35 +22,25 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
     @Bean
 
     public PasswordEncoder passwordEncoder() {
-
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
     }
 
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.userDetailsService(email -> {
-
             Client client = clientRepository.findByEmail(email);
 
             if (client != null) {
                 if (client.getEmail().equals("admin@admin.com")) {
-
                     return new User(client.getEmail(), client.getPassword(),
                             AuthorityUtils.createAuthorityList("ADMIN"));
-
                 } else {
-
                     return new User(client.getEmail(), client.getPassword(),
                             AuthorityUtils.createAuthorityList("CLIENT"));
-
                 }
-
             } else {
-
                 throw new UsernameNotFoundException("Unknown user: " + email);
-
             }
         });
     }
