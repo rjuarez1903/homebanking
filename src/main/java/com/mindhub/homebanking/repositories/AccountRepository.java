ee7885998e48bdfd81e186fe9a64e0995ee44ada
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-03-06 22:12:24 -0300

    add findById method

--- src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
@@ -4,7 +4,10 @@ import com.mindhub.homebanking.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByNumber(String number);
+    Optional<Account> findById(Long id);
 }
